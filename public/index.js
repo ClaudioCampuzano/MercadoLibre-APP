522b7bdebe20f2b4adbbb721707d302fc2220bfe
Claudio <claudio.campuzano.b@gmail.com>
2023-06-13 01:08:23 -0400

    feat: add assets used in project

---
+++ public/index.js
@@ -0,0 +1,7 @@
+import MercadoLibre from './mercadoLibre-logo.svg';
+import Search from './search.svg';
+import ChevronRight from './chevron-right.svg';
+import Spinner from './spinner.svg';
+import Error from './error.svg';
+
+export { MercadoLibre, Search, ChevronRight, Spinner, Error };
