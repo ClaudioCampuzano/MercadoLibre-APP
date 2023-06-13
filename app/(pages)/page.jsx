a367cb3d9ccf109541dd852f3c3c911f4b782c56
Claudio <claudio.campuzano.b@gmail.com>
2023-06-13 05:03:59 -0400

    feat: add view for root url

--- app/(pages)/page.jsx
+++ app/(pages)/page.jsx
@@ -1,7 +1,3 @@
-import React from 'react';
-
-const Home = (props) => {
-  return <div>Home</div>;
-};
+const Home = () => {};
 
 export default Home;
--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import styles from '@/styles/globals.module.scss';
 import { Inter } from 'next/font/google';
+import { SearchBar } from '@/components/SearchBar/SearchBar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,7 +11,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es" className={inter.className}>
-      <body className={styles.body}>{children}</body>
+      <body className={styles.body}>
+        <SearchBar />
+        {children}
+      </body>
     </html>
   );
 }
