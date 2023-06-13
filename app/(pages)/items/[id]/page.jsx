3edd3587c321c190b1513679539d17dba49a94b7
Claudio <claudio.campuzano.b@gmail.com>
2023-06-13 05:05:43 -0400

    feat: add view to product detail mode

--- app/(pages)/items/[id]/page.jsx
+++ app/(pages)/items/[id]/page.jsx
@@ -1,7 +1,34 @@
-import React from 'react';
+'use client';
 
-const Item = (props) => {
-  return <div>Item</div>;
+import React from 'react';
+import { useFetchProductDetail } from '@/api/useFetchProductDetail';
+import styles from './id.module.scss';
+import { Breadcrumb } from '@/components/Breadcrumb/Breadcrumb';
+import { ItemDetail } from '@/components/ItemDetail/ItemDetail';
+import { StatusMessage } from '@/components/StatusMessage/StatusMessage';
+
+const Item = ({ params }) => {
+  const { id } = params;
+  const { product: productData, isLoading, isError } = useFetchProductDetail(id);
+
+  const { item, categories } = productData || {};
+  return (
+    <div className={styles.container}>
+      {isLoading || isError ? (
+        <>
+          {isLoading && <StatusMessage status="loading" />}
+          {isError && <StatusMessage status="error" />}
+        </>
+      ) : (
+        <div className={styles.content2}>
+          <div className={styles.content}>
+            <Breadcrumb pathFromRoute={categories} />
+            <ItemDetail data={item} />
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Item;
