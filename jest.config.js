843730e1b6bdf5184bd10c310cb387560d3838f8
Claudio <claudio.campuzano.b@gmail.com>
2023-06-13 08:21:28 -0400

    chore: add installation and configuration of jest

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const nextJest = require('next/jest');
+const createJestConfig = nextJest({
+  dir: './',
+});
+const customJestConfig = {
+  moduleDirectories: ['node_modules', '<rootDir>/'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>$1',
+  },
+  clearMocks: true,
+};
+module.exports = createJestConfig(customJestConfig);
